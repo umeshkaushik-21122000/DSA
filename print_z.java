3b8d5f26d9af0670a90975776866c7ce555e520c
umeshkaushik-21122000 <79392801+umeshkaushik-21122000@users.noreply.github.com>
2021-04-09 15:37:15 +0530

    Add files via upload

---
+++ print_z.java
@@ -0,0 +1,12 @@
+public class print_z {
+    public static void main(String[] args) {
+
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+
+    }
+}
