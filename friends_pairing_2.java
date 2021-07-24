1a6ad97fae8e3f7e30fe6884fbb4e49ebac1d8be
umeshkaushik-21122000 <79392801+umeshkaushik-21122000@users.noreply.github.com>
2021-07-25 04:54:15 +0530

    Add files via upload

---
+++ friends_pairing_2.java
@@ -0,0 +1,42 @@
+/*
+3
+
+1.(1) (2) (3)
+2.(1) (2,3)
+3.(1,2) (3)
+4.(1,3) (2)
+ */
+import java.util.*;
+public class friends_pairing_2{
+    static int counter = 1;
+    public static void solution(int i, int n, boolean[] used, String asf) {
+        if (i > n) {
+            System.out.println(counter + "." + asf);
+            counter++;
+            return;
+        }
+
+        if (used[i]) {
+            solution(i + 1, n, used, asf);
+        }
+        else {
+            used[i] = true;
+            solution(i + 1, n, used, asf + "(" + i + ") ");
+            for (int j = i + 1; j <= n; j++) {
+                if (!used[j]) {
+                    used[j] = true;
+                    solution(i + 1, n, used, asf + "(" + i + "," + j + ") ");
+                    used[j] = false;
+                }
+            }
+
+            used[i] = false;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] used = new boolean[n + 1];
+        solution(1, n, used, "");
+    }
+}
