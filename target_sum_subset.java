fb8e0c101f3f49acfd5f9285bbe82573d7b20e22
umeshkaushik-21122000 <79392801+umeshkaushik-21122000@users.noreply.github.com>
2021-10-01 14:59:19 +0530

    Add files via upload

--- target_sum_subset.java
+++ target_sum_subset.java
@@ -1,33 +1,84 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class target_sum_subset {
 
-    public static void main(String[] args) throws Exception {
-        Scanner my = new Scanner(System.in);
-        int n = my.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = my.nextInt();
-        }
-        int tar=my.nextInt();
-        printtss(a,0,"",0,tar);
-    }
+    public static class Pair{
+        int i;
+        int j;
+        String psf;
 
-    public static void printtss(int[] a, int idx, String set, int sos, int tar) {
-        if(sos==tar){
-            String z = "";
-            for(int i=idx;i<a.length;i++){
-                if(a[i]==0){
-                    z=z+"0, ";
-                    System.out.println(set+z+".");
+        public Pair(int i, int j, String psf){
+            this.i = i;
+            this.j = j;
+            this.psf = psf;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int tar = Integer.parseInt(br.readLine());
+
+        boolean[][] dp = new boolean[arr.length + 1][tar + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = false;
+                } else if (j == 0) {
+                    dp[i][j] = true;
+                } else {
+                    if(dp[i - 1][j]){
+                        dp[i][j] = true;
+                    } else {
+                        int val = arr[i - 1];
+                        if (j >= val && dp[i - 1][j - val]) {
+                            dp[i][j] = true;
+                        }
+                    }
                 }
             }
-            System.out.println(set+".");
-            return;
         }
-        for(int j=idx;j<a.length;j++)
-        {
-            printtss(a,j+1,set+a[j]+",",sos+a[j],tar);
+
+        System.out.println(dp[dp.length - 1][tar]);
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(n,tar,""));
+
+
+        while (q.size() > 0) {
+            Pair rp = q.remove();
+            if (rp.i == 0 || rp.j == 0) {
+                System.out.println(rp.psf);
+            } else {
+                boolean exc = dp[rp.i - 1][rp.j];
+                boolean inc = rp.j - arr[rp.i - 1] >= 0 && dp[rp.i - 1][rp.j - arr[rp.i - 1]];
+
+                if (inc) {
+                    q.add(new Pair(rp.i - 1, rp.j - arr[rp.i - 1], (rp.i - 1) + " " + rp.psf));
+                }
+                if (exc) {
+                    q.add(new Pair(rp.i - 1, rp.j, rp.psf));
+                }
+            }
         }
     }
 }
+
+
+                        
+
+
+                        
+
+
+                        
+
+
+                        
